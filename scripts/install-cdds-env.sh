8106601f2aa908197157326a5f8af2e976276c91
agstephens <ag.stephens@stfc.ac.uk>
2019-06-14 15:41:43 +0100

    Updated install script

--- scripts/install-cdds-env.sh
+++ scripts/install-cdds-env.sh
@@ -14,7 +14,7 @@ if [ ! $SVN_CDDS_USERNAME ]; then
 fi
 
 if [ ! $SVN_CDDS_PASSWORD ]; then
-    echo "[ERROR] Must set SVN_CDDS_ername environment variable."
+    echo "[ERROR] Must set SVN_CDDS_PASSWORD environment variable."
     exit
 fi
 
@@ -22,7 +22,8 @@ fi
 TOP_DIR=cdds-env
 CDDS_TAG_URL=https://code.metoffice.gov.uk/svn/cdds/main/tags/1.1.0
 ENVS_REPO=https://github.com/cedadev/esgf-conda-envs
-REQUIRED_PACKAGES="cdds_convert"
+REQUIRED_PACKAGES="hadsdk mip_convert cdds_configure cdds_convert cdds_prepare cdds_qc"
+
 GWS=/gws/smf/j04/cmip6_prep
 
 rm -fr $TOP_DIR
